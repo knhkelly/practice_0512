341554f8faede44a1935ad184c2d2d10a8b837bf
knhkelly <knhkelly@gmail.com>
2023-05-12 09:28:29 +0900

    변수 복습

---
+++ index.js
@@ -0,0 +1,12 @@
+var varName = "var";
+let letName = "let";
+const constName = "const";
+
+console.log(varName)
+
+var varName = "var1";
+// var는 재선언도 재할당도 가능
+letName = "let1";
+letName = "let2";
+// let은 선언은 안되지만 재할당은 가능
+// const는 재할당도 안됨
